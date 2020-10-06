a3516c86ddd8f28d04d1155da3fac2d12e3e67ad
Andrew Leshovsky <unwisenerd@gmail.com>
2020-10-07 00:08:59 +0300

    Added main classes and utilities

--- Deck.py
+++ Deck.py
@@ -2,3 +2,27 @@
 # Class for creating and managing game deck
 #
 
+import random
+from Cards import Card
+from Utilities import get_suits, get_ranks
+
+
+def get_deck():
+    deck = []
+    for suit in get_suits():
+        for rank in get_ranks():
+            card = Card(suit, rank)
+            deck.append(card)
+    return deck
+
+
+class Deck:
+
+    def __init__(self):
+        self.all_cards = get_deck()
+
+    def shuffle(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
